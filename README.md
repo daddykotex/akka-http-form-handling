2f8c2189642f89bc71e42445ad5fe824dd03547f
David Francoeur <dfrancoeur04@gmail.com>
2021-11-05 08:38:26 -0400

    feat: directive to stream files and form fields

--- README.md
+++ README.md
@@ -1,9 +1,50 @@
-# Simple example do stream multi-part forms
+# Simple example to stream multi-part forms
 
-Run with `sbt run`
+## Usage
+
+Run with `sbt run`.
+
+Even better, if you use `sbt-revolver` you'll get automatic reloading.
 
 The server will start on `8080` and you can talk to it with curl:
 
 ```
 curl 'http://localhost:8080'
 ```
+
+## Multipart forms
+
+This repository exists as a reference for someone who might have this question:
+
+* I want to get a hold of all the files uploaded in a form as well as the form fields, how can I do that with akka-http.
+
+I have already written about this [here](https://davidfrancoeur.com/post/akka-http-multipart-form/). This is a complex problem.
+
+akka-http has supports for multipart forms via a few directives, see: https://doc.akka.io/docs/akka-http/current/routing-dsl/directives/file-upload-directives/index.html. The downside is that when you use those directives, they consume the request body and discard the fields. You can theorically combine the too, but you will get errors as the request size incread. Why? Because as long as the request is small enough, it's loaded as a `Strict` request (in-memory) and you can use two directive that parse the body without a problem. But when the framework decides it's best not too load it in memory, the second directive parsing the body will fail (in unexpected ways).
+
+In this repo, we'll implement a `/forms` endpoint that does that:
+
+* stream incoming request into our directive
+* file parts are written to disk
+* fields are kept in a map
+* when parsing is complete you get a `Map[String, List[File]]` for files and a `Map[String, List[String]]` for fields.
+
+Open up [http://localhost:8080](http://localhost:8080) in your browser. Use the HTML form to test the endpoint.
+
+You can also use curl, like so:
+
+```bash
+# let's upload three files
+> ls -l1 .| head -3
+collections_2.12-1.2.8.jar
+jawn-parser_2.12-0.10.4.jar
+scala-library-2.12.7.jar
+
+> curl 'http://localhost:8080/form' \
+  -F 'jar=@collections_2.12-1.2.8.jar' \
+  -F 'jar=@jawn-parser_2.12-0.10.4.jar' \
+  -F 'scala=@scala-library-2.12.7.jar' \
+  -F 'version=2'
+```
+
+When you submit a `multipart/form-data`, the implementation will stream the files part to disk, and it will accumulate fields into a `Map`. When using the directive, you'll get a `Map[FileInfo, Vector[File]]` where the key are the file part info (field name, content-type, file name). The value is a `Vector` because a `multipart/form-data` can contain the same file part multiple times. The same thing goes for the fields, where you get a `Map[String, Vector[String]]`
--- src/main/scala/com/daddykotex/FormRoutes.scala
+++ src/main/scala/com/daddykotex/FormRoutes.scala
@@ -1,10 +1,135 @@
 package com.daddykotex
 
+import java.io.File
+import java.util.UUID
+
 import akka.actor.typed.ActorSystem
-import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.server._
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.directives._
+import akka.stream.scaladsl._
+import akka.stream.IOResult
+import akka.util.ByteString
+
+import scala.concurrent.duration._
+import scala.concurrent.Future
+import scala.util.Random
 
 class FormRoutes()(implicit as: ActorSystem[_]) extends Directives {
-  val routes = get {
-    complete("hello")
+  type FormUploadResult = (
+      Map[FileInfo, Vector[File]],
+      Map[String, Vector[String]]
+  )
+  private val emptyForm: FormUploadResult = (Map.empty, Map.empty)
+  private val filesAndFields: Directive[FormUploadResult] =
+    entity(as[Multipart.FormData]).flatMap { formData =>
+      extractRequestContext.flatMap { ctx =>
+        implicit val mat = ctx.materializer
+        implicit val ec = ctx.executionContext
+
+        // on the left side we emit file parts
+        // on the right side we emit field parts
+        val incoming: Source[Either[(FileInfo, File), (String, String)], Any] =
+          formData.parts
+            .mapAsyncUnordered(5) { part =>
+              part.filename match {
+                case Some(value) =>
+                  val fileInfo =
+                    FileInfo(part.name, value, part.entity.contentType)
+                  val prefix = Option(fileInfo.fileName)
+                    .filter(_.nonEmpty)
+                    .getOrElse(Random.nextString(5))
+                  val dest = File.createTempFile(prefix, ".tmp")
+                  part.entity.dataBytes
+                    .runWith(FileIO.toPath(dest.toPath))
+                    .flatMap {
+                      case x if x.wasSuccessful =>
+                        Future.successful(Left(fileInfo -> dest))
+                      case x => Future.failed(x.getError)
+                    }
+                case None =>
+                  part.entity.dataBytes
+                    .runFold(ByteString.empty)(_ ++ _)
+                    .map(_.utf8String)
+                    .map(data => Right(part.name -> data))
+              }
+            }
+
+        // we accumulate everything in two maps
+        val uploadedF: Future[FormUploadResult] =
+          incoming.runWith(Sink.fold(emptyForm) {
+            case ((files, fields), Right(fieldName -> fieldValue)) =>
+              val inserted: Vector[String] =
+                fields
+                  .get(fieldName)
+                  .map(_ :+ fieldValue)
+                  .getOrElse(Vector(fieldValue))
+              (files, fields.updated(fieldName, inserted))
+            case ((files, fields), Left(fileInfo -> file)) =>
+              val inserted: Vector[File] =
+                files.get(fileInfo).map(_ :+ file).getOrElse(Vector(file))
+
+              (files.updated(fileInfo, inserted), fields)
+          })
+
+        onSuccess(uploadedF)
+      }
+    }
+
+  private val htmlForm =
+    (get & pathEndOrSingleSlash) {
+      complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, dummyForm))
+    }
+
+  private val formEndpoint = (post & path("form")) {
+    withoutSizeLimit {
+      filesAndFields { case (files, fields) =>
+        for ((fi, filesPerField) <- files) {
+          for (file <- filesPerField) {
+            println(
+              s"File ${fi.fileName} for field ${fi.fieldName} at ${file.getAbsolutePath()}"
+            )
+          }
+        }
+        for ((field, values) <- fields) {
+          for (v <- values) {
+            println(s"Field $field has value: ${v}")
+          }
+        }
+        complete(StatusCodes.OK)
+      }
+    }
   }
+
+  val routes = htmlForm ~ formEndpoint
+
+  private val dummyForm = """|
+  |<form action="http://localhost:8080/form" method="POST" enctype="multipart/form-data">
+  |  <div>
+  |    <label>11:</label>
+  |    <input type="text" name="field1" />
+  |  </div>
+  |
+  |  <div>
+  |    <label>2:</label>
+  |    <input type="text" name="field2" />
+  |  </div>
+  |
+  |  <div>
+  |    <label>1 again:</label>
+  |    <input type="text" name="field1" />
+  |  </div>
+  |
+  |  <div>
+  |    <label>File 1:</label>
+  |    <input type="file" name="file1" />
+  |  </div>
+  |
+  |  <div>
+  |    <label>File 2:</label>
+  |    <input type="file" name="file2" />
+  |  </div>
+  |
+  |  <button>Submit</button>
+  |</form>""".stripMargin
 }
